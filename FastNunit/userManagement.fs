d24b1c7a4caca39ea98621662b1a1f71fd26b648
Thomas Freeman <tom.1.freeman@nokia.com>
2013-08-13 08:51:18 +0100

    Other: Basic first draft userManagement.

--- FastNunit/userManagement.fs
+++ FastNunit/userManagement.fs
@@ -2,18 +2,74 @@
 
 open System.Security
 
+open System.Net
+open Helpers
+
 open FSharp.Data
 
 type userList = JsonProvider<"userList.json">
 
 type UserManager() =
-    member this.GetFree() =
-        let users = userList.Parse("{}")
-        let user = Seq.head users.Items
-        let pass = user.Password.ToCharArray() |> Seq.fold (fun (sec:SecureString) char -> 
-                                                                sec.AppendChar(char)
-                                                                sec) (new SecureString())
-        Some (user.Username, pass)
 
-    member this.AssignUser(username) = async { return () }
-    member this.FreeUser(username) = async { return () }
+   // Creates a basic agilezen rest request with the correct header
+    let createRequest path = 
+        let root =  "https://localhost:9999/"
+
+        let request = HttpWebRequest.Create(root + path)
+        request :?> HttpWebRequest
+
+    // Makes the get request given a url fragment and a lambda to parse the output
+    let tryMakeGet urlFragment parse = async {
+            let request = createRequest urlFragment
+            
+            use! response = request.AsyncGetResponse()
+            let httpResponse = response :?> HttpWebResponse
+
+            return! match httpResponse.StatusCode with
+                    | HttpStatusCode.OK -> httpResponse.GetResponseStream() 
+                                           |> readStreamAsString 
+                                           |!> parse
+                                           |!> Some
+                    | HttpStatusCode.NotFound -> async { return None }
+                    | _ -> failwith "Bad response"
+    }
+
+    
+
+
+    member this.GetFree() =
+
+        let parse text =
+            let users = userList.Parse(text)
+            let user = Seq.head users.Items
+            let pass = user.Password.ToCharArray() |> Seq.fold (fun (sec:SecureString) char -> 
+                                                                    sec.AppendChar(char)
+                                                                    sec) (new SecureString())
+            user.Name, pass
+
+        tryMakeGet "free/users" parse
+        |> Async.RunSynchronously
+
+    member this.AssignUser(username) = async {             
+            let request = createRequest (sprintf "/users/%s" username)
+            request.Method <- "PUT"
+            
+            use! response = request.AsyncGetResponse()
+            let httpResponse = response :?> HttpWebResponse
+
+            return match httpResponse.StatusCode with
+                   | HttpStatusCode.OK -> ()
+                   | _ -> failwith "Bad response"
+    }
+
+    member this.FreeUser(username) =  async {
+            let request = createRequest (sprintf "/users/%s" username)
+            request.Method <- "DELETE"
+            
+            use! response = request.AsyncGetResponse()
+            let httpResponse = response :?> HttpWebResponse
+
+            return match httpResponse.StatusCode with
+                   | HttpStatusCode.OK -> ()
+                   | _ -> failwith "Bad response"
+    }
---
+++ Packager/Helpers.fs
@@ -0,0 +1,52 @@
+﻿module Helpers
+
+    open System.Net
+
+    let (==) opt1 opt2 =
+        match opt1 with
+        | Some(value1) -> match opt2 with
+                          | Some(value2) -> value1 = value2
+                          | None -> false
+        | None -> opt2.IsNone
+
+    let (!=) opt1 opt2 = not (opt1 == opt2)
+
+    let readStreamAsString (stream:System.IO.Stream) =
+        async {
+            let reader = new System.IO.StreamReader(stream)
+            return! Async.AwaitTask( reader.ReadToEndAsync()) }
+
+    let writeStringToStream (stream:System.IO.Stream) (text:string) =
+        let writer = new System.IO.StreamWriter(stream)
+        writer.Write(text)
+        writer.Flush()
+        writer.Close()
+
+    // Sends the text as the body of the given request
+    let sendBody (request:HttpWebRequest) text = async{
+        try
+            use requestStream = request.GetRequestStream()
+
+            writeStringToStream requestStream text
+
+            let response = request.GetResponse()
+
+            return response :?> HttpWebResponse
+
+        with
+            | ex -> raise ex
+                    return null // this is silly, and may never get hit.
+
+    }
+
+    // redefine |> for async computation expressions
+    let (|!>) asyncF followup = async{
+            let! ans = asyncF
+            return followup ans 
+            }
+
+    // define minus for sequences to be the subset of sequence 1 not in sequence 2
+    let (-) seq1 seq2 =
+        seq1 |> Seq.filter (fun item1 -> seq2 
+                                         |> Seq.exists (fun item2 -> item1 = item2)
+                                         |> not)
